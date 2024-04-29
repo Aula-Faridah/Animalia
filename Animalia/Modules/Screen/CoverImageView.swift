63fb68cafee4b06edb042211a8f11e504c73fa3a
Aula Faridah <aulafaridah@gmail.com>
2024-04-29 11:59:23 +0700

    Implement grid switch to Animal List and Animal Grid

--- Animalia/Modules/Screen/CoverImageView.swift
+++ Animalia/Modules/Screen/CoverImageView.swift
@@ -10,13 +10,26 @@ import SwiftUI
 struct CoverImageView: View {
     let coverImages: [CoverImageModel] = Bundle.main.decode("covers.json")
     
+    @State private var showingSheet = false
+    @State private var selectedCover: CoverImageModel?
+    
     var body: some View {
         TabView {
             ForEach(coverImages) { item in
                 Image(item.name)
                     .resizable()
                     .scaledToFill()
+                    .onTapGesture {
+                        showingSheet.toggle()
+                        selectedCover = item
+                    }
                 
+            }
+            .sheet(isPresented: $showingSheet) {
+                if let selectedCover = selectedCover {
+                    OctagonImageFrame(cover: selectedCover)
+                        .presentationDetents([.medium, .large] )
+                }
             }
         }
         .tabViewStyle(PageTabViewStyle())
