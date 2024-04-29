f78080d02dca0033057088f6d2c5b60fc098dab0
Aula Faridah <aulafaridah@gmail.com>
2024-04-29 09:34:39 +0700

    Add screen CoverImageView

---
+++ Animalia/Modules/Models/CoverImageModel.swift
@@ -0,0 +1,14 @@
+//
+//  CoverImageModel.swift
+//  Animalia
+//
+//  Created by MacBook Pro on 29/04/24.
+//
+
+import Foundation
+
+struct CoverImageModel: Codable, Identifiable {
+    let id: Int
+    let name: String
+    
+}
